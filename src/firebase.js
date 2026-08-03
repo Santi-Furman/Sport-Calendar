4261ec3126b4ddfe30e2d221bb4a0479e19d3deb
“Santi <santifurman@gmail.com>
2026-08-03 01:25:09 -0300

    fix: imports duplicados

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
-// Reemplaza con los datos reales de tu consola de Firebase
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
